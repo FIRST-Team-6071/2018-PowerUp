858d1a714d1908f283c039ca215fad1696e6bfd6
Blake <minecraf7loverr@gmail.com>
2018-01-20 10:36:15 -0500

    Basic TeleOp v1.2
    Same code, just with some updated variables.

--- PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
+++ PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
@@ -14,14 +14,11 @@ import edu.wpi.first.wpilibj.Spark;
 public class Robot extends IterativeRobot {
 	
 	// Common Variables
-	final int joystickLeftPort = 0;
-	final int joystickRightPort = 1;
-	
 	final Spark mtrLeft = new Spark(0);
 	final Spark mtrRight = new Spark(1);
 	final Spark mtrGrabLeft = new Spark(2);
 	final Spark mtrGrabRight = new Spark(3);
-	final Spark mtrVert = new Spark(4);
+	final Spark mtrVertical = new Spark(4);
 	
 	
 	// Auton Variables.
@@ -66,16 +63,14 @@ public class Robot extends IterativeRobot {
 			mtrGrabLeft.setSpeed(1);
 		}
 		if (leftJoy.getRawButton(1)) {
-			mtrGrabLeft.setSpeed(-0.3);
-			mtrGrabRight.setSpeed(-0.3);
+			mtrGrabLeft.setSpeed(-0.5);
+			mtrGrabRight.setSpeed(-0.5);
 		}
 		if (rightJoy.getRawButton(3)) {
-			mtrVert.setSpeed(0.7);
+			mtrVertical.setSpeed(1);
 		}
 		if (leftJoy.getRawButton(4)) {
-			mtrVert.setSpeed(-0.7);
+			mtrVertical.setSpeed(-1);
 		}
-// Please note that the only reason motor speed isn't set to a full +1 and -1 is
-// that we don't want the motors going faster than the robot's balance can handle.
     }
 }
