00677e2b436f073928a0856469f4998cd9420f58
Blake <minecraf7loverr@gmail.com>
2018-01-20 10:29:20 -0500

    Basic TeleOp
    This is all of the basic TeleOp that we have the ability to make so far.  The controls in this version of the code involve being able to pick up and drop boxes, as well as have the elevator go up and down.

--- PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
+++ PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
@@ -17,8 +17,11 @@ public class Robot extends IterativeRobot {
 	final int joystickLeftPort = 0;
 	final int joystickRightPort = 1;
 	
-	final Spark motorsLeft = new Spark(0);
-	final Spark motorsRight = new Spark(1);
+	final Spark mtrLeft = new Spark(0);
+	final Spark mtrRight = new Spark(1);
+	final Spark mtrGrabLeft = new Spark(2);
+	final Spark mtrGrabRight = new Spark(3);
+	final Spark mtrVert = new Spark(4);
 	
 	
 	// Auton Variables.
@@ -47,16 +50,32 @@ public class Robot extends IterativeRobot {
 		double rightJoyVal = rightJoy.getRawAxis(1);
 		
 		if (leftJoyVal < 0) {
-			motorsLeft.setSpeed(leftJoyVal * leftJoyVal * -1);
+			mtrLeft.setSpeed(leftJoyVal * leftJoyVal * -1);
 		}
 		if (leftJoyVal >= 0) {
-			motorsLeft.setSpeed(leftJoyVal * leftJoyVal);
+			mtrLeft.setSpeed(leftJoyVal * leftJoyVal);
 		}
 		if (rightJoyVal < 0) {
-			motorsRight.setSpeed(rightJoyVal * rightJoyVal * -1);
+			mtrRight.setSpeed(rightJoyVal * rightJoyVal * -1);
 		}
 		if (rightJoyVal >= 0) {
-			motorsRight.setSpeed(rightJoyVal * rightJoyVal);
+			mtrRight.setSpeed(rightJoyVal * rightJoyVal);
 		}
+		if (rightJoy.getRawButton(1)) {
+			mtrGrabLeft.setSpeed(1);
+			mtrGrabLeft.setSpeed(1);
+		}
+		if (leftJoy.getRawButton(1)) {
+			mtrGrabLeft.setSpeed(-0.3);
+			mtrGrabRight.setSpeed(-0.3);
+		}
+		if (rightJoy.getRawButton(3)) {
+			mtrVert.setSpeed(0.7);
+		}
+		if (leftJoy.getRawButton(4)) {
+			mtrVert.setSpeed(-0.7);
+		}
+// Please note that the only reason motor speed isn't set to a full +1 and -1 is
+// that we don't want the motors going faster than the robot's balance can handle.
     }
 }
