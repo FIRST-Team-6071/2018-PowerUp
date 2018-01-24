8fc829f8a2edf07ed2079219b62f2789a930557c
Zachary Seebeck <xeinix@outlook.com>
2018-01-24 18:42:32 -0500

    Setup CAN testing.
    Seting up basic motor movement using CAN.

--- PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
+++ PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
@@ -5,28 +5,28 @@
 
 package org.usfirst.frc.team6071.robot;
 
-import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-
-
 
 public class Robot extends IterativeRobot {
 	
 	// Common Variables
-	final Spark mtrLeft = new Spark(0);
-	final Spark mtrRight = new Spark(1);
+	final Spark mtrLeft = new Spark(0); // Left side gearbox.
+	final Spark mtrRight = new Spark(1); // Right side gearbox.
 	final Spark mtrGrabLeft = new Spark(2);
 	final Spark mtrGrabRight = new Spark(3);
 	final Spark mtrVertical = new Spark(4);
 
+	TalonSRX mtrAux = new TalonSRX(0); // Testing, kinda. Will be used for the AUX motor.
 	
 	// Auton Variables.
-	public int staNumber = 1;
+	public int staNumber = 1; 
 	public String arcadeLayout;
 	
 	
@@ -43,11 +43,11 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void robotInit() {
-
+		mtrAux.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		mtrAux.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 	}	
 	
     public void teleopPeriodic() {
-    	
     	double leftJoyVal = leftJoy.getRawAxis(1);
 		double rightJoyVal = rightJoy.getRawAxis(1);
 		
@@ -77,5 +77,11 @@ public class Robot extends IterativeRobot {
 		if (leftJoy.getRawButton(4)) {
 			mtrVertical.setSpeed(-1);
 		}
+		
+		/*
+		 * WARNING! TESTING CODE! NOT EDSIGNED TO GO INTO A FINAL BUILD!
+		 */
+		if (leftJoy.getRawButton(7)) mtrAux.set(ControlMode.PercentOutput, 0.5d); // In theory should move the motor with 50% speed.
+		
     }
 }
