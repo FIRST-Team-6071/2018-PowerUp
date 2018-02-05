533ab937c646d2154aa1e1f14bc8397f410236ee
Zachary Seebeck <xeinix@outlook.com>
2018-02-05 10:12:30 -0500

    Cleanup and some Pneumatics stuff.

--- PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
+++ PowerUp2018/src/org/usfirst/frc/team6071/robot/Robot.java
@@ -17,13 +17,10 @@ public class Robot extends IterativeRobot {
 	// Common Variables
 	final Spark mtrLeft = new Spark(0); // Left side gearbox controller.
 	final Spark mtrRight = new Spark(1); // Right side gearbox controller.
-	final Spark mtrGrabLeft = new Spark(2);
-	final Spark mtrGrabRight = new Spark(3);
-	final Spark mtrVertical = new Spark(4);
 	final Encoder encMtrLeft = new Encoder(5,6); // Left side gearbox encoder.
 	final Encoder encMtrRight = new Encoder(7,8); // Right side gearbox encoder.
-	final Solenoid solBox = new Solenoid(0);
-	final Compressor compCube = new Compressor(0);
+	final Solenoid solBoxPush = new Solenoid(0);
+	final Compressor compressor = new Compressor(0);
 
 	
 	// Auton Variables.
@@ -48,13 +45,16 @@ public class Robot extends IterativeRobot {
 		encMtrRight.setDistancePerPulse(5);
 		encMtrRight.setReverseDirection(true);
 		encMtrRight.setSamplesToAverage(7);
+		
+		compressor.setClosedLoopControl(true);
 	}	
 	
-	@Override
+	@Override 
     public void teleopPeriodic() {
     	double leftJoyVal = leftJoy.getRawAxis(1); // Gets the y position's value from the left joystick.
 		double rightJoyVal = rightJoy.getRawAxis(1); // Gets the x position's value from the right joystick.
-		compCube.start();
+		
+		// Drivetrain code.
 		if (leftJoyVal < 0) {
 			mtrLeft.setSpeed(leftJoyVal * leftJoyVal * -1);
 		}
@@ -67,26 +67,13 @@ public class Robot extends IterativeRobot {
 		if (rightJoyVal >= 0) {
 			mtrRight.setSpeed(rightJoyVal * rightJoyVal);
 		}
+		
+		// Box Pushing Code.
 		if (rightJoy.getRawButton(1)) {
-			ExtendPusher(true);
+			solBoxPush.set(true);
 		}
 		else {
-			ExtendPusher(false);
-		}
-		if (rightJoy.getRawButton(7)) {
-			compCube.start();
-		}
-		else {
-			compCube.stop();
+			solBoxPush.set(false);
 		}
     }
-	
-	private void ExtendPusher(boolean isPushed) {
-		if (isPushed) {
-			solBox.set(true);
-		}
-	    if (isPushed) {
-	    	solBox.set(false);
-	    }// Put solonoid true code here.
-	}
 }
